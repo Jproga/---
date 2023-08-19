5a274cc83702b843e4b4686848101621e810db6b
Jproga <138982927+Jproga@users.noreply.github.com>
2023-08-19 18:32:09 +0300

    мемчо сайт.py

---
+++ мемчо сайт.py
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Тут написано, как называется вкладка в браузере</title>
+    <title>Полезная информация!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Это очень большой заголовок!</h1>
+    <p>Это просто какой-то абзац текста</p>
+    <h2>А это заголовок поменьше</h2>
+             <ul>
+                   <li>А это точечки в списке (Это первая)</li>
+                   <li>А это вторая</li>
+             </ul>
+     <img src="https://otkritkis.com/wp-content/uploads/2022/02/1464729791156456568.jpg" alt="Image 1">
+     <img src="https://privetpeople.ru/23fevr/Spasatel/programmist-34.jpg" alt="Image 2">
+     <img src="https://img2.reactor.cc/pics/post/full/Чилик-Комиксы-it-юмор-geek-5369680.jpeg" alt="Image 3">
+     <img src="https://risovach.ru/upload/2014/03/mem/tipichnyj-nedosypayucshij_46174470_orig_.jpg" alt="Image 4">
+     <img src="https://fikiwiki.com/uploads/posts/2022-02/1644925822_2-fikiwiki-com-p-prikolnie-kartinki-pro-programmistov-2.jpg" alt="Image 5">
+     <img src="https://fikiwiki.com/uploads/posts/2022-02/1644925870_13-fikiwiki-com-p-prikolnie-kartinki-pro-programmistov-13.jpg" alt="Image 6">
+     <img src="http://i.mycdn.me/i?r=AzFIxPtkV78jcmdRfpoIOyaJK2xMcSNx-qDhP5jsv2JIhTO7EuABt8t6j6ROsg0SwU4" alt="Image 7">
+     <img src="https://bipbap.ru/wp-content/uploads/2017/01/udalyonnoe-61033.jpeg" alt="Image 8">
+</body>
+</html>
